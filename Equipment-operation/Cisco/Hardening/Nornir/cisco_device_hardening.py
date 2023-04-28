11c4137b1efeff85387c604a1700e335ad3a0a65
Lai920123 <Lai76219654@gmail.com>
2023-04-28 23:41:07 +0800

    add Hardening

---
+++ Equipment-operation/Cisco/Hardening/Nornir/cisco_device_hardening.py
@@ -0,0 +1,18 @@
+from nornir import InitNornir
+
+nr = InitNornir (
+    runner={
+        "plugin": "threaded",
+        "options": {
+            "num_workers": 100,
+        },
+    },
+    inventory={
+        "plugin": "SimpleInventory",
+        "options": {
+            "host_file": "inventory/hosts.yaml",
+            "group_file": "inventory/groups.yaml",
+            "defaults_file": "inventory/defaults.yaml"
+        }
+    }
+)
